7ed9fbe8ce6bc7fa0bfcbc6cf7f4529ec178d9e4
Dan <dandundg@gmail.com>
2025-08-14 12:07:56 -0600

    Move drawCard function

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,22 @@ function App() {
   const [count, setCount] = useState(0);
   const [cardsHand, setCardsHand] = useState([]);
 
+  const drawCard = () => {
+    setCardsHand((prevCards) => [
+      ...prevCards,
+      <Card key={prevCards.length} type="1" />,
+    ]);
+
+    console.log(cardsHand);
+  };
+
   return (
     <>
       <button onClick={() => setCount((count) => count + 1)}>
         count is {count}
       </button>
 
-      <button
-        onClick={() =>
-          setCardsHand((prevCards) => [
-            ...prevCards,
-            <Card key={prevCards.length} type="1" />,
-          ])
-        }
-      >
-        Draw Card
-      </button>
+      <button onClick={drawCard}>Draw Card</button>
 
       <div className="cards-hand">{cardsHand}</div>
     </>
